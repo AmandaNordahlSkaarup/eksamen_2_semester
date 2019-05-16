84cd513c4cef1c36997d0efd8f6f79ef1b158df3
pernille thieme <pernillethieme@gmail.com>
2019-05-16 11:13:58 +0200

    java valg quiz

---
+++ valgtest/main.js
@@ -0,0 +1,465 @@
+// kode lavet af Christian 
+
+//Spørgsmålene står her 
+let Q1 = document.querySelector(".Q1");
+let Q2 = document.querySelector(".Q2");
+let Q3 = document.querySelector(".Q3");
+let Q4 = document.querySelector(".Q4");
+let Q5 = document.querySelector(".Q5");
+let Q6 = document.querySelector(".Q6");
+let Q7 = document.querySelector(".Q7");
+let Q8 = document.querySelector(".Q8");
+let Q9 = document.querySelector(".Q9");
+let Q10 = document.querySelector(".Q10");
+
+
+//De forskellige svar muligheder 
+let C1 = document.querySelector(".C1");
+let C2 = document.querySelector(".C2");
+let C3 = document.querySelector(".C3");
+let C4 = document.querySelector(".C4");
+let C5 = document.querySelector(".C5");
+let C6 = document.querySelector(".C6");
+let C7 = document.querySelector(".C7");
+let C8 = document.querySelector(".C8");
+let C9 = document.querySelector(".C9");
+let C10 = document.querySelector(".C10");
+let C11 = document.querySelector(".C11");
+let C12 = document.querySelector(".C12");
+let C13 = document.querySelector(".C13");
+let C14 = document.querySelector(".C14");
+let C15 = document.querySelector(".C15");
+let C16 = document.querySelector(".C16");
+let C17 = document.querySelector(".C17");
+let C18 = document.querySelector(".C18");
+let C19 = document.querySelector(".C19");
+let C20 = document.querySelector(".C20");
+
+// Hvad der sker når man klikker på et svar 
+C1.addEventListener("click", C1Fx);
+C2.addEventListener("click", C2Fx);
+C3.addEventListener("click", C3Fx);
+C4.addEventListener("click", C4Fx);
+C5.addEventListener("click", C5Fx);
+C6.addEventListener("click", C6Fx);
+C7.addEventListener("click", C7Fx);
+C8.addEventListener("click", C8Fx);
+C9.addEventListener("click", C9Fx);
+C10.addEventListener("click", C10Fx);
+C11.addEventListener("click", C11Fx);
+C12.addEventListener("click", C12Fx);
+C13.addEventListener("click", C13Fx);
+C14.addEventListener("click", C14Fx);
+C15.addEventListener("click", C15Fx);
+C16.addEventListener("click", C16Fx);
+C17.addEventListener("click", C17Fx);
+C18.addEventListener("click", C18Fx);
+C19.addEventListener("click", C19Fx);
+C20.addEventListener("click", C20Fx);
+
+//
+Q10.style.display = "none";
+Q9.style.display = "none";
+Q8.style.display = "none";
+Q7.style.display = "none";
+Q6.style.display = "none";
+Q5.style.display = "none";
+Q4.style.display = "none";
+Q3.style.display = "none";
+Q2.style.display = "none";
+Q1.style.display = "block";
+
+
+
+//variable  
+let venstre = 0; 
+let konservative = 0; 
+let stramkurs = 0;
+let socialDemokratiet = 0; 
+let alternativet = 0; 
+let SF = 0;
+let radikaleVenstre = 0;
+let klausR = 0;
+let liberalAlliance = 0;
+let enhedesListen = 0;
+let kristenDemokraterne = 0;
+let nyeBorgelige = 0;
+let danskFolkeparti = 0;
+
+
+// spørgsmål 1: DK nok miljøet?
+// Ja 
+function C1Fx(){ 
+	
+   Q10.style.display = "none";
+   Q9.style.display = "none";
+   Q8.style.display = "none";
+   Q7.style.display = "none";
+   Q6.style.display = "none";
+   Q5.style.display = "none";
+   Q4.style.display = "none"; 
+   Q3.style.display = "none";
+   Q2.style.display = "block";
+   Q1.style.display = "none"; 
+   
+   beregnStemmer(0,0,0,0,0,0,0,0,0,0,0,5,5);
+
+}
+// nej 
+function C2Fx(){ 
+    
+	Q10.style.display = "none";
+    Q9.style.display = "none";
+    Q8.style.display = "none";
+    Q7.style.display = "none";
+    Q6.style.display = "none";
+	Q5.style.display = "none";
+	Q4.style.display = "none";
+    Q3.style.display = "none";
+    Q2.style.display = "block";
+    Q1.style.display = "none"; 
+   
+    beregnStemmer(5,5,0,5,5,5,5,5,5,5,5,0,0);
+}
+
+//spørgsmål 2: Klima Afgift?
+//ja 
+function C3Fx(){ 
+    Q10.style.display = "none";
+	Q9.style.display = "none";
+	Q8.style.display = "none";
+	Q7.style.display = "none";
+	Q6.style.display = "none";
+	Q5.style.display = "none";
+	Q4.style.display = "none";
+    Q3.style.display = "block";
+    Q2.style.display = "none";
+    Q1.style.display = "none"; 
+    beregnStemmer(0,2,0,3,5,3,3,4,0,4,4,0,0);
+}
+
+//nej 
+function C4Fx(){ 
+    
+	Q10.style.display = "none";
+	Q9.style.display = "none";
+	Q8.style.display = "none";
+	Q7.style.display = "none";
+	Q6.style.display = "none";
+	Q5.style.display = "none";
+	Q4.style.display = "none";
+    Q3.style.display = "block";
+    Q2.style.display = "none";
+    Q1.style.display = "none"; 
+    beregnStemmer(2,2,3,0,0,0,0,0,4,0,0,5,4);
+}
+
+//spørgsmål 3: Stramninger Udlændinge? 
+//ja 
+function C5Fx(){ 
+    Q10.style.display = "none";
+	Q9.style.display = "none";
+	Q8.style.display = "none";
+	Q7.style.display = "none";
+	Q6.style.display = "none";
+	Q5.style.display = "none";
+	Q4.style.display = "block";
+    Q3.style.display = "none";
+    Q2.style.display = "none";
+    Q1.style.display = "none"; 
+    beregnStemmer(3,3,5,2,0,0,0,3,3,0,0,5,5);
+}
+
+//nej 
+function C6Fx(){
+    
+	Q10.style.display = "none";
+	Q9.style.display = "none";
+	Q8.style.display = "none";
+	Q7.style.display = "none";
+	Q6.style.display = "none";
+	Q5.style.display = "none";
+	Q4.style.display = "block";
+    Q3.style.display = "none";
+    Q2.style.display = "none";
+    Q1.style.display = "none"; 
+    beregnStemmer(2,1,0,2,5,4,5,1,2,5,3,0,0);
+}
+
+//spørgsmål 4: Asylstop?
+//ja 
+function C7Fx(){ 
+    Q10.style.display = "none";
+	Q9.style.display = "none";
+	Q8.style.display = "none";
+	Q7.style.display = "none";
+	Q6.style.display = "none";
+	Q5.style.display = "block";
+	Q4.style.display = "none";
+    Q3.style.display = "none";
+    Q2.style.display = "none";
+    Q1.style.display = "none"; 
+    beregnStemmer(3,4,5,1,0,0,0,2,3,0,0,5,4);
+}
+
+//nej 
+function C8Fx(){ 
+    
+	Q10.style.display = "none";
+	Q9.style.display = "none";
+	Q8.style.display = "none";
+	Q7.style.display = "none";
+	Q6.style.display = "none";
+	Q5.style.display = "block";
+	Q4.style.display = "none";
+    Q3.style.display = "none";
+    Q2.style.display = "none";
+    Q1.style.display = "none"; 
+    beregnStemmer(1,0,0,1,5,4,5,1,2,0,3,0,0);
+}
+
+//spørgsmål 5: Cigaretter?
+//ja 
+function C9Fx(){ 
+    Q10.style.display = "none";
+	Q9.style.display = "none";
+	Q8.style.display = "none";
+	Q7.style.display = "none";
+	Q6.style.display = "block";
+	Q5.style.display = "none";
+	Q4.style.display = "none";
+    Q3.style.display = "none";
+    Q2.style.display = "none";
+    Q1.style.display = "none"; 
+    beregnStemmer(3,4,0,3,5,4,5,0,0,4,5,0,2);
+}
+
+//nej 
+function C10Fx(){ 
+    
+	Q10.style.display = "none";
+	Q9.style.display = "none";
+	Q8.style.display = "none";
+	Q7.style.display = "none";
+	Q6.style.display = "block";
+	Q5.style.display = "none";
+	Q4.style.display = "none";
+    Q3.style.display = "none";
+    Q2.style.display = "none";
+    Q1.style.display = "none"; 
+    beregnStemmer(1,1,2,1,0,1,0,4,5,0,0,4,2);
+}
+
+//spørgsmål 6: uddnnelsesloft? 
+//ja 
+function C11Fx(){ 
+    Q10.style.display = "none";
+	Q9.style.display = "none";
+	Q8.style.display = "none";
+	Q7.style.display = "block";
+	Q6.style.display = "none";
+	Q5.style.display = "none";
+	Q4.style.display = "none";
+    Q3.style.display = "none";
+    Q2.style.display = "none";
+    Q1.style.display = "none"; 
+    beregnStemmer(0,1,0,4,5,5,5,5,4,5,5,2,0);
+}
+
+//nej 
+function C12Fx(){ 
+    
+	Q10.style.display = "none";
+	Q9.style.display = "none";
+	Q8.style.display = "none";
+	Q7.style.display = "block";
+	Q6.style.display = "none";
+	Q5.style.display = "none";
+	Q4.style.display = "none";
+    Q3.style.display = "none";
+    Q2.style.display = "none";
+    Q1.style.display = "none"; 
+    beregnStemmer(5,4,0,0,0,0,0,0,0,0,0,3,5);
+}
+
+//spørgsmål 7: Topskat?
+//ja 
+function C13Fx(){ 
+    Q10.style.display = "none";
+	Q9.style.display = "none";
+	Q8.style.display = "block";
+	Q7.style.display = "none";
+	Q6.style.display = "none";
+	Q5.style.display = "none";
+	Q4.style.display = "none";
+    Q3.style.display = "none";
+    Q2.style.display = "none";
+    Q1.style.display = "none"; 
+    beregnStemmer(3,4,3,2,2,0,0,5,5,1,2,4,1);
+}
+
+//nej 
+function C14Fx(){ 
+    
+	Q10.style.display = "none";
+	Q9.style.display = "none";
+	Q8.style.display = "block";
+	Q7.style.display = "none";
+	Q6.style.display = "none";
+	Q5.style.display = "none";
+	Q4.style.display = "none";
+    Q3.style.display = "none";
+    Q2.style.display = "none";
+    Q1.style.display = "none"; 
+    beregnStemmer(2,1,2,3,4,4,5,0,0,4,4,2,4);
+}
+
+//spørgsmål 8: Kontanthjælpsloft?
+//ja 
+function C15Fx(){ 
+    Q10.style.display = "none";
+	Q9.style.display = "block";
+	Q8.style.display = "none";
+	Q7.style.display = "none";
+	Q6.style.display = "none";
+	Q5.style.display = "none";
+	Q4.style.display = "none";
+    Q3.style.display = "none";
+    Q2.style.display = "none";
+    Q1.style.display = "none"; 
+    beregnStemmer(0,0,0,4,5,5,5,4,2,5,4,2,2);
+}
+
+//nej 
+function C16Fx(){ 
+    
+	Q10.style.display = "none";
+	Q9.style.display = "block";
+	Q8.style.display = "none";
+	Q7.style.display = "none";
+	Q6.style.display = "none";
+	Q5.style.display = "none";
+	Q4.style.display = "none";
+    Q3.style.display = "none";
+    Q2.style.display = "none";
+    Q1.style.display = "none"; 
+    beregnStemmer(5,5,0,1,0,0,0,1,3,0,1,3,3);
+}
+
+//spørgsmål 9: Cannabis? 
+//ja 
+function C17Fx(){ 
+    Q10.style.display = "block";
+	Q9.style.display = "none";
+	Q8.style.display = "none";
+	Q7.style.display = "none";
+	Q6.style.display = "none";
+	Q5.style.display = "none";
+	Q4.style.display = "none";
+    Q3.style.display = "none";
+    Q2.style.display = "none";
+    Q1.style.display = "none"; 
+    beregnStemmer(0,0,0,2,5,5,5,5,4,5,0,2,0);
+}
+
+//nej 
+function C18Fx(){ 
+    
+	Q10.style.display = "block";
+	Q9.style.display = "none";
+	Q8.style.display = "none";
+	Q7.style.display = "none";
+	Q6.style.display = "none";
+	Q5.style.display = "none";
+	Q4.style.display = "none";
+    Q3.style.display = "none";
+    Q2.style.display = "none";
+    Q1.style.display = "none"; 
+    beregnStemmer(5,5,0,3,0,0,0,0,1,0,5,3,2);
+}
+
+//spørgsmål 10: Samtykke?
+//ja 
+function C19Fx(){ 
+    Q10.style.display = "none";
+	Q9.style.display = "none";
+	Q8.style.display = "none";
+	Q7.style.display = "none";
+	Q6.style.display = "none";
+	Q5.style.display = "none";
+	Q4.style.display = "none";
+    Q3.style.display = "none";
+    Q2.style.display = "none";
+    Q1.style.display = "none"; 
+    beregnStemmer(4,4,0,5,5,5,5,3,3,5,2,0,3);
+	 let all = document.querySelector(".allQuestions");
+    all.innerHTML =
+		`
+	Venstre: ${venstre} <br> 
+	Konservative: ${konservative} <br> 
+	Stramkurs: ${stramkurs} <br> 
+	Socialdemokratiet: ${socialDemokratiet} <br> 
+	SF: ${SF} <br> 
+	Alternativet: ${alternativet} <br>
+	radikaleVenstre: ${radikaleVenstre} <br> 
+	klausR: ${klausR} <br> 
+	liberalAlliance: ${liberalAlliance} <br> 
+	enhedesListen: ${enhedesListen} <br> 
+	kristenDemokraterne: ${kristenDemokraterne} <br> 
+	nyeBorgelige: ${nyeBorgelige} <br> 
+	danskFolkeparti: ${danskFolkeparti} <br> 		`
+}
+
+//nej 
+function C20Fx(){ 
+    
+	Q10.style.display = "none";
+	Q9.style.display = "none";
+	Q8.style.display = "none";
+	Q7.style.display = "none";
+	Q6.style.display = "none";
+	Q5.style.display = "none";
+	Q4.style.display = "none";
+    Q3.style.display = "none";
+    Q2.style.display = "none";
+    Q1.style.display = "none"; 
+    beregnStemmer(1,1,0,0,0,0,0,1,2,0,3,2,2);
+	 let all = document.querySelector(".allQuestions");
+    all.innerHTML =
+		`
+	Venstre V: ${venstre} <br> 
+	Det Konservative Folkeparti C: ${konservative} <br> 
+	Stram kurs P: ${stramkurs} <br> 
+	Socialdemokratiet S: ${socialDemokratiet} <br> 
+	SF - Socialitisk Folkeparti F: ${SF} <br> 
+	Alternativet Å: ${alternativet} <br>
+	Radikale Venstre B: ${radikaleVenstre} <br> 
+	Klaus Riskær Pedersen E: ${klausR} <br> 
+	Liberal Alliance I: ${liberalAlliance} <br> 
+	Enhedeslisten Ø: ${enhedesListen} <br> 
+	Kristendemokraterne K: ${kristenDemokraterne} <br> 
+	Nye Borgelige D: ${nyeBorgelige} <br> 
+	Dansk Folkeparti O: ${danskFolkeparti} <br> 		`
+}
+
+
+function beregnStemmer(venstrePoint, konservativePoint, stramkursPoint, socialDemokratietPoint, alternativetPoint, sFPoint, radikaleVenstrePoint, klausRPoint, liberalAlliancePoint, enhedesListenPoint, kristenDemokraternePoint, nyeBorgeligePoint, danskFolkepartiPoint) { 
+
+	venstre =  venstrePoint + venstre;
+    konservative = konservativePoint + konservative;
+    stramkurs = stramkursPoint + konservative;
+    socialDemokratiet = socialDemokratietPoint + socialDemokratiet; 
+	alternativet = alternativetPoint + alternativet;
+	SF = sFPoint + SF;
+	radikaleVenstre = radikaleVenstrePoint + radikaleVenstre; 
+	klausR = klausRPoint + klausR;
+	liberalAlliance = liberalAlliancePoint + liberalAlliance;
+	enhedesListen = enhedesListenPoint + enhedesListen;
+	kristenDemokraterne = kristenDemokraternePoint + kristenDemokraterne;
+	nyeBorgelige = nyeBorgeligePoint + nyeBorgelige;
+	danskFolkeparti = danskFolkepartiPoint + danskFolkeparti;
+
+}
+
+
+
